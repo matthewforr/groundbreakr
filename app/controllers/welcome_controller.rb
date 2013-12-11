2fa2acf1b8ba477466831d9f323c83ca6d68753d
Matthew Forr <matthew.forr@gmail.com>
2013-12-11 09:26:35 -0500

    Cleaned out commented code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,12 +6,6 @@ class WelcomeController < ApplicationController
 
     if current_user
       update_progress_bar
-      # @keeper_user = Runkeeper.new(@current_user.token)
-      # @user = User.find_by_name(@keeper_user.name)
-      # @user_runs = @keeper_user.fitness_activities.parsed_response["items"]
-      # if (@user.run_count < @user_runs.count)
-      #   @user.update_attribute(:run_count, @user_runs.count)
-      # end
     end
   end
 end
